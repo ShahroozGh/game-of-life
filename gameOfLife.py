f6d43b66e520f79bc6602bd77da9d550673ab6a1
Shahrooz Ghayouri <shahrooz.ghayouri@mail.utoronto.ca>
2015-08-12 21:09:18 -0400

    Add cell life and death steps

--- gameOfLife.py
+++ gameOfLife.py
@@ -24,14 +24,19 @@ class game:
 		self.cellList[4][1].live = True
 		self.cellList[0][0].live = True
 
+		self.cellList[20][20].live = True
+		self.cellList[20][21].live = True
+		self.cellList[20][22].live = True
+
 		self.canvas.bind("<Button-1>", self.canvasClicked)
 		
 
 		self.gameLoop(self.cellList)
 
 	def gameLoop(self, cellList):
+		self.checkLivingConditions(cellList)
 		self.paint(cellList)
-		self.root.after(100, self.gameLoop, cellList)
+		self.root.after(10000, self.gameLoop, cellList)
 
 	def paint(self,cellList):
 		self.canvas.delete(tk.ALL)
@@ -46,11 +51,65 @@ class game:
 		xTile = (event.x - (event.x % 10)) / 10
 		yTile = (event.y - (event.y % 10)) / 10
 
-
 		self.addOrKillCell(True,int(xTile), int(yTile),self.cellList)
+		self.paint(self.cellList)
 
 	def addOrKillCell(self,live,x,y , cellList):
 		cellList[x][y].live = live
+
+	def checkLivingConditions(self, cellList):
+		for x in range(70):
+			for y in range(70):
+				liveNeighbours = self.checkNeighbours(x,y,cellList)
+				if cellList[x][y].live is True:
+					
+					if liveNeighbours < 2:
+						#Kill Cell
+						self.addOrKillCell(False,x,y,cellList)
+					elif liveNeighbours > 3:
+						#Kill Cell
+						self.addOrKillCell(False,x,y,cellList)
+					else:
+						#Keep Cell 
+						self.addOrKillCell(True,x,y,cellList)
+
+
+				elif cellList[x][y].live is False:
+
+					if liveNeighbours == 3:
+						#Live Cell
+						self.addOrKillCell(True,x,y,cellList)
+
+
+	def checkNeighbours(self,x,y,cellList):
+		liveNeighbours = 0
+
+		if x+1 < 70 and y+1 < 70:
+			if cellList[x+1][y+1].live == True:
+				liveNeighbours += 1
+		if x-1 > -1 and y-1 > -1: 
+			if cellList[x-1][y-1].live == True:
+				liveNeighbours += 1
+		if x+1 < 70 and y-1 > -1: 
+			if cellList[x+1][y-1].live == True:
+				liveNeighbours += 1
+		if x-1 > -1 and y+1 < 70: 
+			if cellList[x-1][y+1].live == True:
+				liveNeighbours += 1
+		if y+1 < 70: 
+			if cellList[x][y+1].live == True:
+				liveNeighbours += 1
+		if y-1 > -1: 
+			if cellList[x][y-1].live == True:
+				liveNeighbours += 1
+		if x+1 < 70: 
+			if cellList[x+1][y].live == True:
+				liveNeighbours += 1
+		if x-1 > -1: 
+			if cellList[x-1][y].live == True:
+				liveNeighbours += 1
+
+		return liveNeighbours
 
 
 
